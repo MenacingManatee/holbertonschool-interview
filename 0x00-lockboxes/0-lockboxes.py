810fde799ca5026a191fe3f15aba74ef63f4e1c6
MenacingManatee <brett.p.davis101@gmail.com>
2020-12-09 22:58:10 -0600

    Simplifies loop condition

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -11,15 +11,13 @@ def canUnlockAll(boxes):
     keys = boxes[0].copy()
     i = 0
     i2 = 1  # instantiating them with a value to ignore first loop
-    count = 0
-    while count < len(boxes) and i < i2:
+    while i < i2:
         i = len(keys)
         keys2 = keys.copy()
         for item in keys2:
             keys.extend(boxes[item])
         keys = list(set(keys))
         i2 = len(keys)
-        count += 1
     tmp = True
     for i in range(len(boxes)):
         if (i != 0 and i not in keys):
