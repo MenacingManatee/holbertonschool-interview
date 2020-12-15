adce620bd9bb562e507d885c7bbcccc5edc22b39
Brett Davis <brett.p.davis101@gmail.com>
2020-12-15 01:43:48 +0000

    Fixed typos

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include lists.h
+#include <stdlib.h>
+#include "lists.h"
 
 /**
  * insert_node - inserts a number into a sorted singly linked list
  * @head: Head node of the list
  * @number: Number to insert
- * 
+ *
  * Return: address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
