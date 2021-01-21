e346105861cfa500d4e4791dd3681f5288947177
Brett Davis <brett.p.davis101@gmail.com>
2021-01-21 11:19:07 +0000

    Finds function with O(n^2) complexity

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
 
+listint_t *find_prev(listint_t *head, listint_t *next);
 /**
  * is_palindrome - Determines if a linked list is a palindrome
  * @head: pointer to list to be evaluated
@@ -8,7 +9,40 @@
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *last, *first;
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
+	last = *head;
+	first = *head;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	while (first != last && first->next != last)
+	{
+		if (first->n != last->n)
+			return (0);
+		first = first->next;
+		last = find_prev(*head, last);
+	}
+	if (first == last || first->n == last->n)
+		return (1);
 	return (0);
+}
+/**
+ * find_prev - Finds the previous node
+ * @head: Pointer to the head node
+ * @next: Node to find the previous of
+ *
+ * Return: Previous node, or NULL
+ */
+listint_t *find_prev(listint_t *head, listint_t *next)
+{
+	while (head != NULL)
+	{
+		if (head->next == next)
+			return (head);
+		head = head->next;
+	}
+	return (NULL);
 }
