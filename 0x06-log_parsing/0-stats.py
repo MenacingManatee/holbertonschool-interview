19955ff5490ca824d6d6ca60d5b2d4c5e565c0b7
Brett Davis <brett.p.davis101@gmail.com>
2021-01-26 07:20:50 +0000

    Prints stats on parsed logs

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import sys
+
+def stats(inp, count, size, dic={}):
+    '''Parses logs sent to file'''
+    data = inp.split( )
+    size += int(data[8])
+    if dic.get(data[7]) == None:
+        dic.update({data[7]: 1})
+    else:
+        dic.update({data[7]: dic.get(data[7]) + 1})
+    if count % 10 == 9:
+        printData(size, dic)
+    return size, dic
+
+
+def printData(size, data):
+    '''Prints the parsed data'''
+    print("File size: {:d}".format(size))
+    tmp = list(data.keys())
+    tmp.sort()
+    for item in tmp:
+        print("{}: {}".format(item, data.get(item)))
+
+
+count = 0
+size = 0
+data = {}
+try:
+    for line in sys.stdin:
+        size, data = stats(line, count, size, data)
+        count += 1
+    sys.stdout.flush()
+finally:
+    printData(size, data)
+    sys.stdout.flush()
+exit(0)
