80a401c958c38c12afff5a836246cf155d471a82
Brett Davis <brett.p.davis101@gmail.com>
2021-03-15 11:00:34 +0000

    Calling it for the night TODO: using too much memory, program is auto-killing. Need to cut down the recursion, something is looping way too much Attack checks don't seem to be working right, loops might be trying to use the same item

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -4,6 +4,43 @@
 import sys
 
 
+def attackCheck(currSol):
+    ''' '''
+    if len(currSol) <= 1:
+        return False
+
+    res = False
+    for item1 in currSol:
+        for item2 in currSol:
+            if item1 is item2:
+                continue
+            if (item2[1] == item1[1] or item2[0] == item1[0]):
+                #print("straight", currSol)
+                res = True
+            elif item1[0] - item2[0] == item1[1] - item2[1]:
+                #print("diagonal", currSol, item1[0], item1[1], item2[0], item2[1])
+                res = True
+    return res
+
+
+def solver(currSol, n, col, start):
+    if len(currSol) == 0:
+        col = start
+    if len(currSol) >= n:
+        return True, currSol
+    if col > n:
+        return True, currSol
+
+    for i in range(n):
+        if not attackCheck(currSol):
+            currSol.append(list([col, i]))
+            if solver(currSol, n, col + 1, 0)[0] == True:
+                return True, currSol
+            else:
+                currSol.remove([col, i])
+    return False, currSol
+
+
 '''Places n non-attacking queens on a nxn sized board'''
 if len(sys.argv) <= 1:
     print("Usage: nqueens N")
@@ -14,3 +51,13 @@ elif not sys.argv[1].isnumeric():
 elif int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
+start = 0
+solutions = []
+solution = solver([], int(sys.argv[1]), 0, start)
+#print(solution)
+while solution[0] == True:
+    solutions.append(solution[1].copy())
+    start += 1
+    solution = solver([], int(sys.argv[1]), 0, start)
+for item in solutions:
+    print(item)
