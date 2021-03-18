0c82ddc7583f65f1f443e2e0cd92f0bc7640b0d5
Brett Davis <brett.p.davis101@gmail.com>
2021-03-18 11:27:41 +0000

    Successfully works for n = 4

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -2,10 +2,8 @@
 
 
 import sys
-import json
 
 
-lmnop = 1
 def attackCheck(currSol):
     ''' '''
     if len(currSol) <= 1:
@@ -18,26 +16,33 @@ def attackCheck(currSol):
                 continue
             if (item2[1] == item1[1] or item2[0] == item1[0]):
                 res = True
-            elif item1[0] - item2[0] == item1[1] - item2[1]:
+            elif (abs(item1[0] - item2[0]) == abs(item1[1] - item2[1])):
                 res = True
     return res
 
 
 def solver(currSol, n, col, start):
     if len(currSol) == 0:
-        col = start
-    if len(currSol) >= n:
+        s = start
+    else:
+        s = 0
+    '''if len(currSol) >= n:
         return True, currSol
-    if col > n:
-        return True, currSol
+    if col >= n:
+        return True, currSol'''
 
-    for i in range(n):
-        if not attackCheck(currSol):
-            currSol.append(list([i, col]))
+    for i in range(s, n):
+        if col == i:
+            continue
+        if attackCheck(currSol) is not True:
+            currSol.append(list([col, i]))
+            if len(currSol) > n:
+                currSol.remove([col, i])
+                return True, currSol
             if solver(currSol, n, col + 1, 0)[0] is True:
                 return True, currSol
             else:
-                currSol.remove([i, col])
+                currSol.remove([col, i])
     return False, currSol
 
 
@@ -51,7 +56,7 @@ elif not sys.argv[1].isnumeric():
 elif int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
-start = 0
+start = 1
 solutions = []
 solution = solver([], int(sys.argv[1]), 0, start)
 while solution[0] is True and solution[1] != []:
@@ -59,4 +64,5 @@ while solution[0] is True and solution[1] != []:
     start += 1
     solution = solver([], int(sys.argv[1]), 0, start)
 for item in solutions:
-    print(item)
+    if len(item) == int(sys.argv[1]):
+        print(item)
