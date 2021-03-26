08f0c8fc099b614e39be6a310fee4f1bda629e7e
Brett Davis <brett.p.davis101@gmail.com>
2021-03-26 02:53:38 +0000

    Betty formatting

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -18,7 +18,14 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 	root = fill_nodes(0, size, NULL, array);
 	return (root);
 }
-avl_t *create_node(int val) {
+/**
+ * create_node - Creates an avl node
+ * @val: Value for node to hold
+ *
+ * Return: Newly made node
+ */
+avl_t *create_node(int val)
+{
 	avl_t *new = malloc(sizeof(avl_t));
 
 	if (new == NULL)
@@ -29,9 +36,21 @@ avl_t *create_node(int val) {
 	new->right = NULL;
 	return (new);
 }
-avl_t *fill_nodes(size_t start, size_t end, avl_t *node, int *array) {
-	avl_t * new;
+/**
+ * fill_nodes - fills all nodes of an avl node
+ *
+ * @start: start value
+ * @end: end value
+ * @node: parent of node to fill
+ * @array: Array of ints containing tree values
+ *
+ * Return: Filled out node
+ */
+avl_t *fill_nodes(size_t start, size_t end, avl_t *node, int *array)
+{
+	avl_t *new;
 	int i = (start + end) / 2;
+
 	if (start >= end || i <= (int)start || i > (int)end)
 		return (NULL);
 	new = create_node(array[i - 1]);
