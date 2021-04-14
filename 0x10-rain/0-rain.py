1795e64d89d76872d3449dbcc46181d5f3d7cb30
Brett Davis <brett.p.davis101@gmail.com>
2021-04-14 06:17:00 +0000

    Changes how function works, need to add functionality for larger basins

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -10,22 +10,11 @@ def rain(walls):
     if len(walls) == 0:
         return (0)
     saved = 0
-    i = 0
+    i = 1
     while i < len(walls) - 1:
-        tmp = run(walls, i)
-        saved += tmp[0]
-        i = tmp[1]
+        tmp = min(walls[i - 1], walls[i + 1])
+        tmp -= walls[i]
+        if (tmp > 0):
+            saved += tmp
+        i += 1
     return (saved)
-
-
-def run(walls, index):
-    ''' Runs ahead to help calculate reserved water '''
-    for i in range(index + 1, len(walls)):
-        if (walls[i] >= walls[index]):
-            if (index == 0 and i == len(walls) - 1):
-                return ((i - index) * walls[index], i)
-            return ((i - index) - 1) * walls[index], i
-    if (index + 1 == len(walls)):
-        return (0, index)
-    else:
-        return (min(walls[index], walls[index + 1]), index + 1)
