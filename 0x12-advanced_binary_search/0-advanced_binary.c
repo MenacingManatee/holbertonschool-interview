9b7cb5cd06f51939cd976c000cf0f97770f0bc22
Brett Davis <brett.p.davis101@gmail.com>
2021-04-27 01:41:55 +0000

    Populates directory

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers.
+ * @array: pointer to the first element of the array to search in
+ * @size: Num of elements in array
+ * @value: value to search for
+ *
+ * Return: Index where value is found, or -1 on failure
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	return (-1);
+}
