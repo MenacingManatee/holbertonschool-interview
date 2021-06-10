f66ab8fcae027dacf5e64661e57dc35f63bd4b91
Brett Davis <brett.p.davis101@gmail.com>
2021-06-10 01:00:56 +0000

    Populates dir with templates

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * merge_sort - sorts an array using merge sort algo
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Return: void
+ */
+void merge_sort(int *array, size_t size)
+{
+	if (size <= 0)
+		array = (void *)array;
+	return;
+}
