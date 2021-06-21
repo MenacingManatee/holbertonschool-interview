07cebcbd8dc02d27a71570a4876bddda094d79bd
Brett Davis <brett.p.davis101@gmail.com>
2021-06-21 21:33:24 +0000

    Calculates total coins needed O(n) Big O

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''determine the fewest number of coins needed to meet a given amount total'''
+
+
+def makeChange(coins, total):
+    '''
+    Usage: makeChange(coins, total)
+    Return: fewest number of coins needed to meet total
+    '''
+    count = 0
+    coins.sort()
+    coins.reverse()
+    for coin in coins:
+        if total / coin >= 1:
+            count += total // coin
+            total -= coin * (total // coin)
+    if total == 0:
+        return count
+    return -1
