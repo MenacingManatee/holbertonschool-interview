63bb972688f91a50d0c9e5b68071bab508a65447
Brett Davis <brett.p.davis101@gmail.com>
2021-07-08 00:34:35 +0000

    populates directory with mandatory files

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * radix_sort - sorts an array of integers in ascending order
+ * using the Radix algo
+ *
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Return: Void
+ */
+void radix_sort(int *array, size_t size)
+{
+
+}
