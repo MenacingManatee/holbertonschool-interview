fb376f2dc0345b26355789daeccf45fe90b81a9a
Brett Davis <brett.p.davis101@gmail.com>
2021-07-13 07:29:34 +0000

    Big O O(n^2)

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Takes a matrix of ints and returns the perimeter
+'''
+
+
+def island_perimeter(grid):
+    '''
+    grid is a list of list of integers:
+    0 represents water
+    1 represents land
+    '''
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if i - 1 < 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i + 1 == len(grid) or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j - 1 < 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j + 1 == len(grid[0]) or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
+                
