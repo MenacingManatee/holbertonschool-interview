9c011ce8f98c48cc44ece84ab05a9cb718bc634d
Brett Davis <brett.p.davis101@gmail.com>
2021-07-20 02:36:44 +0000

    Added prototype to h file

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -21,6 +21,8 @@ typedef struct binary_tree_s binary_tree_t;
 
 typedef struct binary_tree_s avl_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *root, size_t val);
+
 void binary_tree_print(const binary_tree_t *tree);
 
 int binary_tree_is_avl(const binary_tree_t *tree);
